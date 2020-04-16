f7d2a772b966a63874a1807d6b8cc734c38576a1
Paulo Saturnino <paulo.saturnino@frk-agency.com>
2020-04-16 09:42:43 +0200

    read files separate in a promise nd wit to calculate

--- react-backend/routes/csvdata.js
+++ react-backend/routes/csvdata.js
@@ -9,12 +9,12 @@ class CSVData {
   
   localFiles = new Array('public/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv', 'public/csse_covid_19_time_series/time_series_covid19_deaths_global.csv', 'public/csse_covid_19_time_series/time_series_covid19_recovered_global.csv');
 
-  countries = [];
+  selectedCountries = [];
   startAt = 0;
 
-  constructor (countries, interval) {
-    if (!countries) countries = ["all"]
-    this.countries = countries
+  constructor (selectedCountries, interval) {
+    if (!selectedCountries) selectedCountries = ["all"]
+    this.selectedCountries = selectedCountries
 
     this.startAt = interval?interval:0
   }
@@ -38,7 +38,7 @@ class CSVData {
       element = element.split('"').join('');
       element_ = element.split(",")
       
-      if (i && element_[1]) {
+      if (element_[1]) {
         if (countries.indexOf(element_[1]) == -1) countries.push(element_[1])
       }
       i++;
@@ -52,7 +52,7 @@ class CSVData {
 
     let days = [], j=0, i=0
     
-    let element_ = stack[0].split(",")
+    let element_ = stack.split(",")
     
     element_.shift()
     element_.shift()
@@ -77,7 +77,7 @@ class CSVData {
     
     let stackResults = [], stackTotals = [], stackDailyIncrease = []
     
-    this.countries.forEach(country => {
+    this.selectedCountries.forEach(country => {
 
       stackResults[c] = []
       stackTotals[c] = []
@@ -143,37 +143,92 @@ class CSVData {
 
   
   
-  readCsvData() {
-    let data = new Array()
+  readCSVData() {
      
-    if (!fs.existsSync(this.getFile(0).local) || !fs.existsSync(this.getFile(1).local) || !fs.existsSync(this.getFile(2).local))  return [];
+    return new Promise((resolve, reject) => {
 
-    let dataArrayTotalCases = []
+      if (!fs.existsSync(this.getFile(0).local) || !fs.existsSync(this.getFile(1).local) || !fs.existsSync(this.getFile(2).local)) reject(false);
+
+      /*let dataArrayTotalDays;
+      
+      let dataArrayTotalCases;
+      await readCSVFile(this.getFile(0).local).then((res) => {dataArrayTotalCases = res;}).catch(() => {});
+      dataArrayTotalCases = dataArrayTotalCases.split(/\r?\n/);
+      dataArrayTotalDays = dataArrayTotalCases[0];
+      dataArrayTotalCases.shift()
+      
+      let dataArrayTotalDeaths;
+      await readCSVFile(this.getFile(1).local).then((res) => {dataArrayTotalDeaths = res}).catch(() => {});
+      dataArrayTotalDeaths = dataArrayTotalDeaths.split(/\r?\n/);
+      dataArrayTotalDeaths.shift()
+      
+      let dataArrayTotalRecovered;
+      await readCSVFile(this.getFile(2).local).then((res) => {dataArrayTotalRecovered = res}).catch(() => {});
+      dataArrayTotalRecovered = dataArrayTotalRecovered.split(/\r?\n/);
+      dataArrayTotalRecovered.shift()*/
+
+      let dataArrayTotalCases, dataArrayTotalDays, dataArrayTotalDeaths, dataArrayTotalRecovered;
+      try {
+        dataArrayTotalCases = fs.readFileSync(this.getFile(0).local, 'utf8');
+        dataArrayTotalCases = dataArrayTotalCases.split(/\r?\n/);
+        dataArrayTotalDays = dataArrayTotalCases[0];
+        dataArrayTotalCases.shift() //because of get Days, the first row hve the days
+      } catch (error) {
+        reject(false);
+      }
+        
+      
+      
+      try {
+        dataArrayTotalDeaths = fs.readFileSync(this.getFile(1).local, 'utf8');
+        dataArrayTotalDeaths = dataArrayTotalDeaths.split(/\r?\n/);
+        dataArrayTotalDeaths.shift()
+      } catch (error) {
+        reject(false);
+      }
+
+      try {
+        dataArrayTotalRecovered = fs.readFileSync(this.getFile(2).local, 'utf8');
+        dataArrayTotalRecovered = dataArrayTotalRecovered.split(/\r?\n/);
+        dataArrayTotalRecovered.shift()
+      } catch (error) {
+        reject(false);
+      }
+      
+      resolve([dataArrayTotalDays, dataArrayTotalCases, dataArrayTotalDeaths, dataArrayTotalRecovered]);
+    })
     
-    dataArrayTotalCases = fs.readFileSync(this.getFile(0).local, 'utf8');
-    dataArrayTotalCases = dataArrayTotalCases.split(/\r?\n/);
-
-    //dataArrayTotalCases.shift() //because of get Days, the first row hve the days
   
+    
+    /*let dataArrayTotalCases = fs.readFileSync(this.getFile(0).local, 'utf8');
+    dataArrayTotalCases = dataArrayTotalCases.split(/\r?\n/);
+    let dataArrayTotalDays = dataArrayTotalCases[0];
+    dataArrayTotalCases.shift() //because of get Days, the first row hve the days
+
     let dataArrayTotalDeaths = fs.readFileSync(this.getFile(1).local, 'utf8');
     dataArrayTotalDeaths = dataArrayTotalDeaths.split(/\r?\n/);
-    
     dataArrayTotalDeaths.shift()
   
     let dataArrayTotalRecovered = fs.readFileSync(this.getFile(2).local, 'utf8');
     dataArrayTotalRecovered = dataArrayTotalRecovered.split(/\r?\n/);
+    dataArrayTotalRecovered.shift()*/
     
-    dataArrayTotalRecovered.shift()
-  
+    //return [dataArrayTotalDays, dataArrayTotalCases, dataArrayTotalDeaths, dataArrayTotalRecovered];
     
-    data[0] = this.getCountries(dataArrayTotalCases)
-    data[1] = this.getDays(dataArrayTotalCases)
-    
-    data[2] = this.getDailyAmount(dataArrayTotalCases, data[1].length)
-    data[3] = this.getDailyAmount(dataArrayTotalDeaths, data[1].length)
-    data[4] = this.getDailyAmount(dataArrayTotalRecovered, data[1].length)
+  }
 
-    data[5] = this.countries
+  calcCSVData(days, total, deaths, recovered) {
+    
+    let data = new Array()
+
+    data[0] = this.getCountries(total)
+    data[1] = this.getDays(days)
+
+    data[2] = this.getDailyAmount(total, data[1].length)
+    data[3] = this.getDailyAmount(deaths, data[1].length)
+    data[4] = this.getDailyAmount(recovered, data[1].length)
+
+    data[5] = this.selectedCountries
     
     return data
   }
@@ -181,61 +236,100 @@ class CSVData {
 }
 
 
+function readCSVFile(path) {
+
+  /*let results = [];
+
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path)
+      .pipe(csv({ separator: ',' }))
+      .on('data', (data) => results.push(data))
+      .on('end', () => {
+        resolve(results);
+    });
+  })*/
+
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', function (err, data) {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+}
 
 
 function handleRequest(req, res) {
-  let interval = "", countries = ""
+  let interval = "", selectedCountries = ""
   
   if (req.query && req.query.interval != null) interval = req.query.interval
-  if (req.params && req.params.id != null) countries = req.params.id.split(",")
+  if (req.params && req.params.id != null) selectedCountries = req.params.id.split(",")
 
-  CSVData_ = new CSVData(countries, interval);
+  CSVData_ = new CSVData(selectedCountries, interval);
 
   if (req.query && req.query.updatedata != null) {
 
-      let promises = []
-      let file = [];
-      for (let index = 0; index < CSVData_.localFiles.length; index++) {
-        promises[index] = new Promise((resolve, reject) => {
-          file[index] = fs.createWriteStream(CSVData_.localFiles[index]);
-          https.get(CSVData_.remoteFiles[index], function (response) {
-              response.pipe(file[index] );
-              file[index].on('finish', function () {
-                resolve(1)
-                file[index].close(); // close() is async, call callback after close completes.
-              });
-              file[index].on('error', function (err) {
-                fs.unlink(CSVData_.localFiles[index]); // Delete the file async. (But we don't check the result)
-                reject(2)
-              });
-          });
-        })
-      }
+    let promises = []
+    let file = [];
+    for (let index = 0; index < CSVData_.localFiles.length; index++) {
+      promises[index] = new Promise((resolve, reject) => {
+        file[index] = fs.createWriteStream(CSVData_.localFiles[index]);
+        https.get(CSVData_.remoteFiles[index], function (response) {
+            response.pipe(file[index] );
+            file[index].on('finish', function () {
+              file[index].close(); // close() is async, call callback after close completes.
+              if (file[index].bytesWritten < 1000) reject(false)
+              else resolve(true)
+            });
+            file[index].on('error', function (err) {
+              fs.unlink(CSVData_.localFiles[index]); // Delete the file async. (But we don't check the result)
+              reject(false)
+            });
+        });
+      })
+    }
 
+    Promise.all(promises)
+    .then((r) => {
       
-      Promise.all(promises)
-      .then(() => {
-        
-        new Promise((resolve, reject) => {
-          const result = (CSVData_.readCsvData())
+      new Promise((resolve, reject) => {
+        CSVData_.readCSVData()
+        .then((res) => {
+          result = CSVData_.calcCSVData(res[0], res[1], res[2], res[3])
           if (result.length) resolve(JSON.stringify(result))
           else reject()
         })
-        .then((out) => res.send(out))
-        .catch(() => res.sendStatus(500))
-
-      }).catch(() => {
-        res.sendStatus(500)
+        .catch(() => {
+          reject()
+        })
       })
+      .then((out) => res.send(out))
+      .catch(() => res.sendStatus(500))
+
+    }).catch((r) => {
+      res.sendStatus(500)
+    })
       
     return
   }
   
+
   
   new Promise((resolve, reject) => {
-    const result = (CSVData_.readCsvData())
-    if (result.length) resolve(JSON.stringify(result))
-    else reject()
+    
+    CSVData_.readCSVData()
+    .then((res) => {
+      result = CSVData_.calcCSVData(res[0], res[1], res[2], res[3])
+      if (result.length) resolve(JSON.stringify(result))
+      else reject()
+    })
+    .catch(() => {
+      reject()
+    })
+    
+    //if (result.length) resolve(JSON.stringify(result))
+    //else reject()
   })
   .then((out) => res.send(out))
   .catch(() => res.sendStatus(500))
