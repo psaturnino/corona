b614844646680c31d708e8c276cf0881cb934563
Paulo Saturnino <psaturnino@gmail.com>
2022-03-07 08:50:13 +0100

    test

--- server/app.js
+++ server/app.js
@@ -20,7 +20,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use(function(req, res, next) {
   //console.log(req.get("referer"))
   const allowedOrigins = [
